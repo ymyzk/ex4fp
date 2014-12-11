1a80cf2f47186edc15a117506e6ad7e388b725a7
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-12-11 14:34:20 +0900

    Add ML2

---
+++ 2/ml2/syntax.ml
@@ -0,0 +1,16 @@
+(* ML interpreter / type reconstruction *)
+type id = string
+
+type binOp = Plus | Mult | Lt | Land | Lor
+
+type exp =
+    Var of id
+  | ILit of int
+  | BLit of bool
+  | BinOp of binOp * exp * exp
+  | IfExp of exp * exp * exp
+  | LetExp of id * exp * exp
+
+type program =
+    Exp of exp
+  | Decl of id * exp
