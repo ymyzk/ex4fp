314888ca1f09926e5b580f180761a7a3f71666dd
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-01-08 16:06:16 +0900

    Update type inference

--- 3/syntax.ml
+++ 3/syntax.ml
@@ -27,9 +27,27 @@ type ty =
   | TyVar of tyvar
   | TyFun of ty * ty
 
-let pp_ty = function
-    TyInt  -> print_string "int"
-  | TyBool -> print_string "bool"
+let string_of_ty ty =
+  let counter = ref 0 in
+  let vars = ref [] in
+  let string_of_tyvar tyvar =
+    let rec index_of_tyvar pos = function
+        [] -> vars := !vars @ [tyvar]; pos
+      | x :: rest -> if x = tyvar then pos
+                     else index_of_tyvar (pos + 1) rest in
+  "'" ^ String.make 1 (char_of_int ((int_of_char 'a') + index_of_tyvar 0 !vars)) in
+  let rec string_of_ty = function
+    TyInt -> "int"
+  | TyBool -> "bool"
+  | TyVar v -> string_of_tyvar v
+  | TyFun (ty1, ty2) ->
+      let str1 = string_of_ty ty1 in
+      let str2 = string_of_ty ty2 in
+      str1 ^ " -> " ^ str2
+  in string_of_ty ty
+
+let pp_ty ty =
+  print_string (string_of_ty ty)
 
 let fresh_tyvar =
   let counter = ref 0 in
--- 3/typing.ml
+++ 3/typing.ml
@@ -8,78 +8,68 @@ let err s = raise (Error s)
 type tyenv = ty Environment.t
 type subst = (tyvar * ty) list
 
+let rec subst_type sub t = match t with
+    TyInt | TyBool -> t
+  | TyFun (ty1, ty2) -> TyFun (subst_type sub ty1, subst_type sub ty2)
+  | TyVar ty -> (match sub with
+      [] -> t
+    | (tyv, ty') :: subt ->
+        if tyv = ty then subst_type subt ty'
+        else subst_type subt t)
+
+let eqs_of_subst s =
+  List.map (fun (tvar, ty) -> (TyVar tvar, ty)) s
+
+let subst_eqs s eqs =
+  List.map (fun (t1, t2) -> (subst_type s t1, subst_type s t2)) eqs
+
+(* (ty * ty) list -> subst *)
+let rec unify l = match l with
+    [] -> []
+  | (ty1, ty2) :: t -> match ty1, ty2 with
+      _, _ when ty1 = ty2 -> unify t
+    | TyVar v, ty3
+    | ty3, TyVar v ->
+        if MySet.member v (freevar_ty ty3) then err ("Error: unify ty")
+        else (v, ty3) :: (unify (subst_eqs [(v, ty3)] t))
+    | TyFun (ty11, ty12), TyFun (ty21, ty22) ->
+        unify ((ty11, ty21) :: (ty12, ty22) :: t)
+    | _ -> err ("Error: unify")
+
 let ty_prim op ty1 ty2 = match op with
-    Plus -> (match ty1, ty2 with
-                TyInt, TyInt -> TyInt
-              | _ -> err ("Argument must be of integer: +"))
-  | Mult -> (match ty1, ty2 with
-                TyInt, TyInt -> TyInt
-              | _ -> err ("Argument must be of integer: *"))
-  | Lt -> (match ty1, ty2 with
-                TyInt, TyInt -> TyBool
-              | _ -> err ("Argument must be of integer: <"))
-  | Land -> (match ty1, ty2 with
-                TyBool, TyBool -> TyBool
-              | _ -> err ("Argument must be of boolean: &&"))
-  | Lor -> (match ty1, ty2 with
-                TyBool, TyBool -> TyBool
-              | _ -> err ("Argument must be of boolean: ||"))
-  | _ -> err ("Not Implemented!")
+    Plus | Mult -> ([(ty1, TyInt); (ty2, TyInt)], TyInt)
+  | Lt -> ([(ty1, TyInt); (ty2, TyInt)], TyBool)
+  | Land | Lor -> ([(ty1, TyBool); (ty2, TyBool)], TyBool)
 
 let rec ty_exp tyenv = function
     Var x ->
-    (try Environment.lookup x tyenv with
+    (try ([], Environment.lookup x tyenv) with
       Environment.Not_bound -> err ("Variable not bound: " ^ x))
-  | ILit _ -> TyInt
-  | BLit _ -> TyBool
+  | ILit _ -> ([], TyInt)
+  | BLit _ -> ([], TyBool)
   | BinOp (op, exp1, exp2) ->
-      let tyarg1 = ty_exp tyenv exp1 in
-      let tyarg2 = ty_exp tyenv exp2 in
-        ty_prim op tyarg1 tyarg2
+      let (s1, ty1) = ty_exp tyenv exp1 in
+      let (s2, ty2) = ty_exp tyenv exp2 in
+      let (eqs3, ty) = ty_prim op ty1 ty2 in
+      let eqs = (eqs_of_subst s1) @ (eqs_of_subst s2) @ eqs3 in
+      let s3 = unify eqs in (s3, subst_type s3 ty)
   | IfExp (exp1, exp2, exp3) ->
-      let tyarg1 = ty_exp tyenv exp1 in
-      (match tyarg1 with
-        | TyBool -> (let tyarg2 = ty_exp tyenv exp2 in
-                     let tyarg3 = ty_exp tyenv exp3 in
-                     match tyarg2, tyarg3 with
-                        TyInt, TyInt -> TyInt
-                      | TyBool, TyBool -> TyBool
-                      | _ -> err ("Type error: if"))
-        | _ -> err ("Test expression must be boolean: if"))
+      let (s1, ty1) = ty_exp tyenv exp1 in
+      let (s2, ty2) = ty_exp tyenv exp2 in
+      let (s3, ty3) = ty_exp tyenv exp3 in
+      let eqs = (eqs_of_subst s1) @ (eqs_of_subst s2) @ (eqs_of_subst s3) @ [(ty1, TyBool); (ty2, ty3)] in
+      let s4 = unify eqs in (s4, subst_type s4 ty2)
   | LetExp (id, exp1, exp2) ->
-      let tyarg1 = ty_exp tyenv exp1 in
-      ty_exp (Environment.extend id tyarg1 tyenv) exp2
+      let (s1, ty1) = ty_exp tyenv exp1 in
+      let (s, ty) = ty_exp (Environment.extend id ty1 tyenv) exp2 in
+      let eqs = (eqs_of_subst s) @ (eqs_of_subst s1) in
+      let s2 = unify eqs in (s2, subst_type s2 ty)
+  | FunExp (id, exp) ->
+      let domty = TyVar (fresh_tyvar ()) in
+      let s, ranty = ty_exp (Environment.extend id domty tyenv) exp in
+      (s, TyFun (subst_type s domty, ranty))
   | _ -> err ("Not Implemented!")
 
 let ty_decl tyenv = function
     Exp e -> ty_exp tyenv e
   | _ -> err ("Not Implemented!")
-
-let rec subst_type sub ty0 = match ty0 with
-    TyInt -> TyInt
-  | TyBool -> TyBool
-  | TyFun (ty1, ty2) -> TyFun (subst_type sub ty1, subst_type sub ty2)
-  | TyVar v -> (match sub with
-      [] -> err ("Error")
-    | s :: subt -> (match s with
-        tyv, t -> if tyv = v then subst_type sub t else subst_type subt ty0))
-
-let rec unify l = match l with
-    [] -> []
-  | (ty1, ty2) :: t ->
-      if ty1 = ty2 then
-        unify t
-      else
-        (match ty1, ty2 with
-           TyVar v, _ ->
-             if MySet.member v (freevar_ty ty2) then
-               err ("Error")
-             else
-               (ty1, ty2) :: unify (List.map (fun (t1, t2) -> (subst_type [(v, ty2)] t1, subst_type [(v, ty2)] t2)) l)
-         | _, TyVar v ->
-             if MySet.member v (freevar_ty ty1) then
-               err ("Error")
-             else
-               (ty2, ty1) :: unify (List.map (fun (t1, t2) -> (subst_type [(v, ty1)] t1, subst_type [(v, ty1)] t2)) l)
-         | TyFun (t11, t12), TyFun (t21, t22) -> unify ((t11, t21) :: (t12, t22) :: t)
-         | _ -> err ("Error"))
